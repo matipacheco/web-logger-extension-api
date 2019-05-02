0ed2387c328d4f15d040402bf9d3b883a0606455
Mati <matipacheco@gmail.com>
2019-05-02 17:25:49 -0400

    variable assignment was missing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require("express");
 const app     = express();
 
 app.post("/log", (request, response) => {
-  database = openConnection();
+  let database = openConnection();
   closeConnection(database);
   response.send("POST received!");
 });
