3303f655fa47445fcb5cc2dea0175180df50b532
Mati <matipacheco@gmail.com>
2019-05-02 17:57:45 -0400

    the way it should be

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-import {
-  openConnection,
-  closeConnection
-} from "./db/database_conector";
-
 const express = require("express");
 const app     = express();
 
-app.post("/log", (request, response) => {
-  let database = openConnection();
-  closeConnection(database);
+let databaseHandler = require("./db/database_handler");
+
+app.get("/", (request, response) => {
+  response.send(databaseHandler.selectAll());
+});
+
+app.post("/register_log", (request, response) => {
+  //databaseHandler.insert(request.body.....)
   response.send("POST received!");
 });
+
+
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
